73c5f37156c1067867b32e6523cea999e69928a8
Ayush Singh <ayushmoca@gmail.com>
2024-04-09 08:06:04 +0530

    feat(header): create header component and add bootstrap styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
-      <h3>We're almost there.</h3>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h3>We're almost there.</h3>
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
+  );
 };
 
 export default App;
