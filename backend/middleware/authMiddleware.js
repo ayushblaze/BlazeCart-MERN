2e1b6f93092e0f5fa25baa1c60115fb985d319df
ayushblaze <ayushmoca@gmail.com>
2024-09-30 18:49:17 +0530

    fix(auth): add error to param list

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 
-const protect = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next, error) => {
   let token;
 
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
